65d0fa5551791a0409c8eda90ab0b9fa526bb9e8
HossamCSE <166309300+HossamCSE@users.noreply.github.com>
2025-04-28 04:41:34 +0300

    Initial commit - Upload Game Launcher project

---
+++ src/gamelauncher/package javaapplication51;.java
@@ -0,0 +1,21 @@
+package javaapplication51;
+
+/**
+ *
+ * @author hos19
+ */
+public class JavaApplication51 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 1; i <= 6; i++) { 
+for (int j = 6; j >= 1; j--) 
+System.out.print(j <= i ? j + " " : " " + " "); 
+System.out.println(); 
+    }
+    
+}
+}
